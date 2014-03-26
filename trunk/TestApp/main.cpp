6c2eb0850b2216c640a1e4a04b67c8ba5b1c1a74
gkupce@gmail.com <gkupce@gmail.com@3d839508-14ca-9b8c-632d-3abf448a3ad5>
2014-03-26 01:30:34 +0000

    Created two simple projects engine and a TestApp that uses it

---
+++ trunk/TestApp/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+#include <test_lib.h>
+
+using namespace std;
+
+void main()
+{
+	cout << "test" << endl;
+	cout << "1+2=" << suma(1,2) << endl;
+
+	system("pause");
+}
---
+++ trunk/engine/include/test_lib.h
@@ -0,0 +1,6 @@
+#ifndef __TEST_LIB__
+#define __TEST_LIB__
+
+int __declspec(dllexport) suma(int i, int j);
+
+#endif //__TEST_LIB__
