4301bf0daf1f8cd274b5a10ff68a7f0ddffe65ba
gkupce@gmail.com <gkupce@users.noreply.github.com>
2014-05-27 00:58:46 +0000

    barely nothing done on the importer and texture headers

---
+++ src/Engine/includes/Importer.h
@@ -0,0 +1,16 @@
+#ifndef __IMPORTER_H__
+#define __IMPORTER_H__
+
+namespace Stu
+{
+	namespace Engine
+	{
+		class Importer
+		{
+		private:
+		public:
+		}
+	}
+}
+
+#endif //__IMPORTER_H__
--- src/Engine/includes/Texture.h
+++ src/Engine/includes/Texture.h
@@ -1,1 +1,16 @@
- 
+#ifndef __TEXTURE_H__
+#define __TEXTURE_H__
+
+namespace Stu
+{
+	namespace Engine
+	{
+		class Texture
+		{
+		private:
+		public:
+		}
+	}
+}
+
+#endif //__TEXTURE_H__
