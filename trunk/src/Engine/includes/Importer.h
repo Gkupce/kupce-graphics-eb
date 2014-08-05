ed2f1f5e2a42ca06616af4e1d4b0bd2f3feebecb
gkupce@gmail.com <gkupce@gmail.com@3d839508-14ca-9b8c-632d-3abf448a3ad5>
2014-08-05 05:12:53 +0000

    Transport commit Started work to import Tilemaps Tilesets might be fully loaded into memory if everything was done correctly.

--- trunk/src/Engine/includes/Importer.h
+++ trunk/src/Engine/includes/Importer.h
@@ -15,6 +15,7 @@ namespace Stu
 	namespace Engine
 	{
 		class Game;
+		class Tilemap;
 
 		class ENGINE_API Importer
 		{
@@ -22,16 +23,19 @@ namespace Stu
 			Importer(Game* game);
 			~Importer();
 			bool LoadResource(const char* dataPath);
-			bool LoadTileMap(const char* fileName);
+			bool LoadTileMap(const char* dataPath);
 			Sprite* GetSprite(const char* name);
+			Tilemap* GetTileMap(const char* name);
 		private:
 			std::map<std::string, Texture::Ptr> moTextureMap;
 			std::map<std::string, Sprite*> moSpriteMap;
+			std::map<std::string, Tilemap*> moTilemapMap;
 			Game* mpoGame;
 
 			bool LoadAnimation(const XMLNode& node, const char* fileName);
 			bool LoadTexture(const char* fileName);
 			bool LoadSprite(const XMLNode& node, const char* fileName);
+			bool CreateTexture(const char* path, const char* name, Color colorKey, int height, int width);
 			
 			std::string getPath(const char* fileName);
 		};
