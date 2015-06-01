6c0e8c559b15ffdd493d76f1007129950dac3f52
gkupce <gkupce@gmail.com>
2015-06-01 11:45:04 -0300

    transport commit. bare-bones bsp work.

--- src/Engine/BspPlane.cpp
+++ src/Engine/BspPlane.cpp
@@ -1,3 +1,4 @@
+#include <math.h>
 #include "includes\BspPlane.h"
 
 Stu::Engine::BspPlane::BspPlane(Vector3 normal, Vector3 point)
@@ -23,6 +24,11 @@ Stu::Engine::BspPlane::BspPlane(Vector3 normal, Vector3 point, std::string name)
 
 int Stu::Engine::BspPlane::checkSide(const Node* node) const
 {
+	//TODO get max and min position and compare both to the plane, if both are on the same side, return the side else return 0
+	//TODO FML
+
+	//int max = sign((node->moMaxCoord + moPoint).Dot(moNormal));
+	//int min = sign((node->moMinCoord + moPoint).Dot(moNormal));
 	return 0;
 }
 
@@ -32,10 +38,24 @@ int Stu::Engine::BspPlane::checkSide(const BspPlane* plane) const
 	Vector3 myNormal = this->moNormal.Normalized();
 	if(otherNormal == myNormal || otherNormal == -myNormal)
 	{//the planes are parallel
-		return 1;
+		//TODO FML get position world
+		return sign( ( (plane->moPoint + plane->GetPosition()) - (moPoint + GetPosition()) ).Dot(moNormal) );
 	}
 	else
 	{//the planes will eventually cross themselves
 		return 0;
 	}
+}
+
+int sign(float number)
+{//TODO make a better, more optimized sign function
+	if(number > 0.001f)
+	{
+		return 1;
+	}
+	else if (number < 0.001f)
+	{
+		return -1;
+	}
+	else return 0;
 }
